0d6a8098ceadedcc4b7ac8b17a8b0ee07fca46cd
Todd Ruhlen <rhythmtome@yahoo.com>
2016-03-01 13:18:43 -0500

    Initialize repository with Chapter 2's code

---
+++ combined.ex
@@ -0,0 +1,8 @@
+#convenience functions!
+defmodule Combined do
+  import Convert
+
+  def height_to_mph(meters) do #takes meters, returns mph
+    Drop.fall_velocity(meters) |> mps_to_mph
+  end
+end
