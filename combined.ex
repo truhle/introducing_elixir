ca3c4f3ef5559ed84e6a16c299dbd7656a78ad13
Todd Ruhlen <rhythmtome@yahoo.com>
2016-03-02 15:01:36 -0500

    Finish ch. 5

--- combined.ex
+++ combined.ex
@@ -3,6 +3,6 @@ defmodule Combined do
   import Convert
 
   def height_to_mph(body \\ :earth, meters) do #takes meters, returns mph
-    Drop.fall_velocity({body, meters}) |> mps_to_mph
+    Drop.fall_velocity(body, meters) |> mps_to_mph
   end
 end
--- drop.ex
+++ drop.ex
@@ -24,7 +24,7 @@ defmodule Drop do
     fall_velocity(planemo, distance)
   end
 
-  defp fall_velocity(planemo, distance) do
+  def fall_velocity(planemo, distance) do
     gravity = case planemo do
       :earth when distance >= 0 -> 9.8
       :moon when distance >= 0 -> 1.6
